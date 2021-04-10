30ab15115c66c547164246c6e94bf893f9a5454f
ling <l13789060033@163.com>
2021-04-10 16:05:57 +0800

    优化css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,7 @@
 <template>
-  <button @click="toggle" :class="{checked:value}"><span></span></button>
+  <button @click="toggle" :class="{checked:value}">
+    <span></span>
+  </button>
 </template>
 
 <script lang="ts">
@@ -27,31 +29,35 @@ button{
   background: #bfbfbf;
   border-radius: $h/2;
   position: relative;
-}
-span{
-  position: absolute;
-  top: 2px;
-  left: 2px;
-  height: $h2;
-  width: $h2;
-  background: white;
-  border-radius: $h2 / 2;
-  transition: all 250ms;
+
+  > span{
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all 250ms;
+
+  }
+  &.checked{
+    background: #1890ff;
+  }
+  &.checked> span {
+    left: calc(100% - #{$h2} - 2px);
+  }
+  &:focus{
+    outline: none;
+  }
+  &:active{
+    > span{ width: $h2 + 4px;}
+  }
+  &.checked:active{
+    > span{width: $h2 + 4px; margin-left: -4px;}
+  }
 
 }
-button.checked{
-  background: #1890ff;
-}
-button.checked> span {
-  left: calc(100% - #{$h2} - 2px);
-}
-button:focus{
-  outline: none;
-}
-button:active{
-  > span{ width: $h2 + 4px;}
-}
-button.checked:active{
-  > span{width: $h2 + 4px; margin-left: -4px;}
-}
+
+
 </style>

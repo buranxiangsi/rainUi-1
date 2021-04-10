8fd3ae48e0d85ede25198184857d9c89cff6d35d
ling <l13789060033@163.com>
2021-04-10 16:27:52 +0800

    Button组件2

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,12 +1,20 @@
 <template>
   <div>Button示例</div>
   <h1>示例</h1>
-  <Button>你好</Button>
+  <Button @click="onClick"
+          @focus="onClick"
+          @mouseover="onClick">Hello</Button>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default {
-  components:{Button}
+  components:{Button},
+  setup(){
+    const onClick = ()=>{
+      console.log('Hi')
+    }
+    return{onClick}
+  }
 }
 </script>

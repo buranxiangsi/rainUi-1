4d92fa4fb596f772668607ed05286a0f7cbafa97
ling <l13789060033@163.com>
2021-04-10 16:57:45 +0800

    支持theme属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,9 +2,9 @@
   <div>Button示例</div>
   <h1>示例</h1>
   <Button>你好</Button>
-  <Button theme="button">你好</Button>
-  <Button theme="link">你好</Button>
-  <Button theme="text">你好</Button>
+  <Button theme="button">button</Button>
+  <Button theme="link">link</Button>
+  <Button theme="text">text</Button>
 </template>
 
 <script lang="ts">

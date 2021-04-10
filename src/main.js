438ab5e089667024ef6977aedf27a5a4559f9c61
ling <l13789060033@163.com>
2021-04-10 11:28:38 +0800

    html结构

--- src/main.js
+++ src/main.js
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import {createRouter, createWebHashHistory} from "vue-router";
+import Home from './views/Home.vue'
+import Doc from  './views/Doc.vue'
 
-createApp(App).mount('#app')
+const history = createWebHashHistory()
+const router = createRouter({
+    history: history,
+    routes:[
+        {path:'/', component: Home},
+        {path:'/doc', component: Doc}
+    ]
+})
+
+const  app = createApp(App)
+app.use(router)
+app.mount('#app')

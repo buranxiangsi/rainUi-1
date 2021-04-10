b0274833560bdf2ebd010a279c39347bffad4f6b
ling <l13789060033@163.com>
2021-04-10 17:54:30 +0800

    支持level属性

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -5,14 +5,46 @@
   <Button theme="link">link</Button>
   <Button theme="text">text</Button>
   <h1>示例2</h1>
-  <Button size="big">big</Button>
-  <Button >normal</Button>
-  <Button size="small">small</Button>
+  <div>
+    <div>
+      <Button size="big">大大大</Button>
+      <Button>普普通</Button>
+      <Button size="small">小小小</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大大大</Button>
+      <Button theme="link">普普通</Button>
+      <Button size="small" theme="link">小小小</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大大大</Button>
+      <Button theme="text">普普通</Button>
+      <Button size="small" theme="text">小小小</Button>
+    </div>
+  </div>
+  <h1>示例3</h1>
+  <div>
+    <div>
+      <Button level="main">main button</Button>
+      <Button >normal button</Button>
+      <Button level="danger">danger button</Button>
+    </div>
+    <div>
+      <Button theme="link" level="main">main link</Button>
+      <Button theme="link">normal link</Button>
+      <Button theme="link" level="danger">danger link</Button>
+    </div>
+    <div>
+      <Button theme="text" level="main">main text</Button>
+      <Button theme="text">normal text</Button>
+      <Button theme="text" level="danger">danger text</Button>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
 import Button from "../lib/Button.vue"
 export default {
-  components:{Button},
+  components:{ Button },
 }
 </script>
--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -16,14 +16,19 @@ export default {
       type: String,
       default: "normal",
     },
+    level:{
+      type: String,
+      default:"normal",
+    },
 
   },
   setup(props){
-    const { theme, size } = props
+    const { theme, size, level } = props
     const classes = computed(()=>{
       return {
         [`raindrop-theme-${theme}`]: theme,
         [`raindrop-size-${size}`]: size,
+        [`raindrop-level-${level}`]: level,
       }
     })
     return { classes }
@@ -37,6 +42,7 @@ $border-color: #d9d9d9;
 $color: #333;
 $blue: #40a9ff;
 $radius: 4px;
+$red: red;
 .raindrop-button{
   box-sizing: border-box;
   height: $h;
@@ -51,7 +57,8 @@ $radius: 4px;
   border: 1px solid $border-color;
   border-radius: $radius;
   box-shadow: 0 1px 0 fade-out(black, 0.95);
-  & + &{
+  transition: background 250ms;
+  & + & {
     margin-left: 8px;
   }
   &:hover,
@@ -79,19 +86,65 @@ $radius: 4px;
     color: inherit;
     &:hover,&:focus {
       background: darken(white, 5%);
-
       }
     }
-  &.raindrop-theme-button{
-    &.raindrop-size-big{
-      font-size: 24px;
-      height: 48px;
-      padding: 0 16px;
+  &.raindrop-size-big{
+    font-size: 24px;
+    height: 48px;
+    padding: 0 16px;
+  }
+  &.raindrop-size-small{
+    font-size: 12px;
+    height: 20px;
+    padding: 0 4px;
+  }
+  &.raindrop-theme-button {
+    &.raindrop-level-main {
+      background: $blue;
+      color: white;
+      border-color: $blue;
+      &:hover,
+      &:focus {
+        background: darken($blue, 10%);
+        border-color: darken($blue, 10%);
+      }
     }
-    &.raindrop-size-small{
-      font-size: 12px;
-      height: 20px;
-      padding: 0 4px;
+
+    &.raindrop-level-danger {
+      background: $red;
+      border-color: $red;
+      color: white;
+      &:hover,
+      &:focus {
+        background: darken($red, 10%);
+        border-color: darken($red, 10%);
+      }
+    }
+  }
+
+  &.raindrop-theme-link {
+    &.raindrop-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red,10%);
+      }
+    }
+  }
+  &.raindrop-theme-text{
+    .rain-level-main{
+      color: $blue;
+      &:hover,
+      &:focus {
+        color: darken($blue,10%);
+      }
+    }
+    &.raindrop-level-danger {
+      color: $red;
+      &:hover,
+      &:focus {
+        color: darken($red,10%);
+      }
     }
   }
 }

939b4eaaf0d7918f900d2ec27065340f997653d2
ling <l13789060033@163.com>
2021-04-10 16:19:23 +0800

    Button组件

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -1,5 +1,12 @@
 <template>
-  <div>
-    Button
-  </div>
+  <div>Button示例</div>
+  <h1>示例</h1>
+  <Button>你好</Button>
 </template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue"
+export default {
+  components:{Button}
+}
+</script>

41fa84de01f08bef87951bb7256f9385a9a16e1d
ling <l13789060033@163.com>
2021-04-10 14:18:54 +0800

    switch组件 css

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -3,7 +3,22 @@
 </template>
 
 <style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button{
+  height: $h;
+  width: $h * 2;
+  border: none;
+  border-radius: $h/2;
+  position: relative;
+}
 span{
-
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
 }
 </style>

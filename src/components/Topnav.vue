2dd31b1c6c26ac321e4354cac6238152b1723087
ling <l13789060033@163.com>
2021-04-11 15:52:48 +0800

    更换按钮

--- src/components/Topnav.vue
+++ src/components/Topnav.vue
@@ -10,12 +10,20 @@
         <router-link to="/doc">文档</router-link>
       </li>
     </ul>
-    <span class="toggleAside" @click="toggleMenu"></span>
+    <svg v-if="toggleMenuButtonVisible" class="toggleAside" @click="toggleMenu">
+      <use xlink:href="#icon-rain-3"></use>
+    </svg>
   </div>
 </template>
 <script lang="ts">
 import { inject, Ref } from 'vue'
 export default {
+  props:{
+    toggleMenuButtonVisible:{
+      type: Boolean,
+      default: false
+    }
+  },
   setup(){
     const menuVisible = inject<Ref<boolean>>('menuVisible') // get
     const toggleMenu = ()=>{
@@ -56,14 +64,14 @@ $color: #007974;
   }
   >.toggleMenu{}
   >.toggleAside{
-    width: 24px;
-    height: 24px;
-    background: red;
+    width: 32px;
+    height: 32px;
     position: absolute;
     left: 16px;
     top: 50%;
     transform: translateY(-50%);
     display: none;
+    background: fade_out(black,0.9);
   }
     @media (max-width: 500px) {
       >.menu{display: none;}

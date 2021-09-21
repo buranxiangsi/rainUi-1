6c94a14499e41b23540bd227274c7f3b98d429a8
buranxiangsi <79569583+buranxiangsi@users.noreply.github.com>
2021-09-21 17:07:31 +0800

    deploy

---
+++ precache-manifest.586a9170bf1cb5d3ea8318a014c2559c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b31d964f4a858e31e9e",
+    "url": "/vmoneny-2/css/app.6c2369bd.css"
+  },
+  {
+    "revision": "e1ed05b6c62c1fdba30dabcdf4000e67",
+    "url": "/vmoneny-2/index.html"
+  },
+  {
+    "revision": "5b31d964f4a858e31e9e",
+    "url": "/vmoneny-2/js/app.6b8b0b56.js"
+  },
+  {
+    "revision": "ccdfae860ce55e270c7d",
+    "url": "/vmoneny-2/js/chunk-vendors.795eede8.js"
+  },
+  {
+    "revision": "75e307defacad44e68a2e2f02dda503d",
+    "url": "/vmoneny-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vmoneny-2/robots.txt"
+  }
+]);

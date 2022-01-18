af695a7f3df5106122481694535eeb9c5a7472ca
ling <l13789060033@163.com>
2022-01-18 16:51:06 +0800

    更改html css

--- src/lib/Radio.vue
+++ src/lib/Radio.vue
@@ -1,10 +1,11 @@
 <template>
-<label class="raindrop-radio">
-  <span class="raindrop-input">
-    <span class="raindrop-input-inner">
-    </span>
+<label :class="wrapClass"  >
+  <span :class="radioClass">
+    <span :class="innerClass"></span>
      <input type="radio" 
-            class="raindrop-input-original"
+            :class="inputClass"
+         
+            
             
 
             
@@ -16,26 +17,67 @@
 </template>
 
 <script lang="ts">
-// import { computed } from "vue"
+import { computed } from "vue"
 export default {
   props:{
+    label:{},
+    value: {},
+    modelValue:[String, Number, Boolean],
+    checked:Boolean,
+    trueValue:{
+      type: Boolean,
+      default: true
+    },
+    falseValue:{
+      type: Boolean,
+      default: false
+    }
+    
+    
+
   },
-  setup(){
-  },
+  setup(props,context){
+    console.log(props)
+    const prefixCls = 'raindrop-radio'
+    const wrapClass = computed(()=>{
+      return[
+        `${prefixCls}-wrapper`,
+        {
+        }
+      ]
+    })
+    const radioClass = computed(()=>{
+      return [
+        `${prefixCls}`,
+        {
+        }
+      ]
+    })
+    const innerClass = computed(()=>{
+      return [
+        `${prefixCls}-inner`,
+        {[`${prefixCls}-inner-checked`]:props.trueValue,}
+      ]
+    })
+    const inputClass = computed(()=>{
+      return [
+        `${prefixCls}-input`
+      ]
+    })
+  }
 }
 </script>
 
 <style lang="scss">
-.raindrop-radio {
+.raindrop-radio-wrapper {
     margin-right: 30px;
-    border: 1px solid red;
     line-height: 1;
     position: relative;
     display: inline-block;
     white-space: nowrap;
     cursor: pointer;
     font-weight: 500;
-    .raindrop-input{
+    .raindrop-radio{
       white-space: nowrap;
       line-height: 1;
       cursor: pointer;
@@ -43,8 +85,7 @@ export default {
       position: relative;
       vertical-align: middle;
       margin-right: 10px;
-    
-      .raindrop-input-inner{
+      .raindrop-radio-inner{
         position: relative;
         border-radius: 100%;
         border: 1px solid #dcdfe6;
@@ -54,9 +95,11 @@ export default {
         cursor: pointer;
         display: inline-block;
         transition: all .2s ease-in-out;
-        
       }
-      .raindrop-input-original{
+      .raindrop-radio-inner-checked{
+        border:5px solid rgb(24, 144, 255);
+      }
+      .raindrop-radio-input{
         opacity: 0;
         outline: none;
         position: absolute;
@@ -69,7 +112,9 @@ export default {
 
 
       }
- } 
+    } 
+    
 }
+
 
 </style>
